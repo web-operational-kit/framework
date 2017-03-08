3e8e3597652a48642d74f2730083033329ae30c6
Sébastien ALEXANDRE <sebastien@graphidev.fr>
2017-03-08 14:40:39 +0100

    Give a try to scripts

---
+++ src/Scripts/Composer/Composer.php
@@ -0,0 +1,48 @@
+<?php
+
+    namespace Scripts\Composer;
+
+    use Composer\Script\Event;
+    use Composer\Installer\PackageEvent;
+
+    class Composer {
+
+        /**
+         * Create a new project instance
+         * ---
+        **/
+        static public function createProjectInstance(Event $event) {
+
+            var_dump(
+                $event->getName(),
+                $event->getArguments()
+            );
+
+        }
+
+        // public static function install(Event $event) {
+        //
+        //     // $package = $event->getOperation()->getPackage();
+        //     $extra = $event->getComposer()->getPackage()->getExtra();
+        //
+        //     $packages = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+        //
+        //     $installManager = $event->getComposer()->getInstallationManager();
+        //
+        //     var_dump(
+        //         $extra,
+        //         $event->getName(),
+        //         $event->getArguments()
+        //     );
+        //     // do stuff
+        //
+        // }
+        //
+        // public static function update(Event $event) {
+        //
+        //     self::install($event);
+        //
+        // }
+
+
+    }
